e9f18ee5f9a238d3afe5e978891225b1ad9b5325
WhereIsMsXi <whereismsxi@gmail.com>
2024-08-22 11:31:46 +0800

    添加方法注释

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,11 +1,13 @@
 import { KeyString } from "../types";
 
+// 统一处理 Promise 中的 Error
 export function to<T>(promise: Promise<T>) {
   return promise
     .then((res: T) => [null, res])
     .catch((err: Error) => [err, null]);
 }
 
+// 处理接口传参时空值可能导致的覆盖问题
 export function removeEmptyProps<
   T extends KeyString
 >(data: T) {
