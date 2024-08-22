fe3505f74130c9fe851597df99f10d46d90764a5
WhereIsMsXi <whereismsxi@gmail.com>
2024-08-22 11:18:09 +0800

    更新 utils.removeEmptyProps 方法

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,20 @@
+import { KeyString } from "../types";
+
 export function to<T>(promise: Promise<T>) {
   return promise
     .then((res: T) => [null, res])
     .catch((err: Error) => [err, null]);
+}
+
+export function removeEmptyProps<
+  T extends KeyString
+>(data: T) {
+  function isEmpty(value: any) {
+    return !['', null, undefined].includes(value)
+  }
+
+  const result: T = { ...data }
+  return Object.fromEntries(
+    Object.entries(result).filter(([key, value]) => isEmpty(value))
+  );
 }
---
+++ tests/utils/removeEmptyProps.test.ts
@@ -0,0 +1,22 @@
+import { removeEmptyProps } from '../../src/utils/utils';
+
+describe('utils removeEmptyProps', () => {
+  it('不能删除对象的非空属性', async () => {
+    const params = {
+      key: 'value'
+    }
+    const result = await removeEmptyProps(params);
+    expect(result).toEqual(params);
+  });
+  it('删除对象空属性', async () => {
+    const params = {
+      key: 'value',
+      name: ''
+    }
+    const target = {
+      key: 'value',
+    }
+    const result = await removeEmptyProps(params);
+    expect(result).toEqual(target);
+  });
+});
